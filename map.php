1363b89b87e57d37950eb1891fda50d5ae9d9f9d
LAymeric <aymeric.larvet@gmail.com>
2019-03-18 19:57:27 +0100

    affichage d'un itinéraire

--- map.php
+++ map.php
@@ -9,40 +9,11 @@ include "navbar.php";
     <input name="end" type="text" id="end"/>
     <button type="button" onclick="searchAddress()">Rechercher</button>
     <div id="map" style="width:100%; height:500px; margin-top:20px"></div>
-    <script>
 
-        function searchAddress() {
-            var start = document.getElementById("start");
-            var end = document.getElementById("end");
-            var requestStart = {
-                query: start.value,
-                fields: ['name']
-            };
-            var requestEnd = {
-                query: end.value,
-                fields: ['name', 'geometry']
-            };
-            var service = new google.maps.places.PlacesService(map);
-
-            service.findPlaceFromQuery(requestStart, function(results, status) {
-                if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    start.value = results[0].name
-                    console.log(results[0].name)
-                }else {
-                    alert("no result for start")
-                }
-            });
-            service.findPlaceFromQuery(requestEnd, function(results, status) {
-                if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    end.value = results[0].name
-                }else {
-                    alert("no result for end")
-                }
-            });
-        }
-    </script>
     <script>
         var map;
+        var directionsService;
+        var directionsDisplay;
         //todo mettre dans un fichier js séparé
         // Initialize and add the map
         function initMap() {
@@ -64,6 +35,54 @@ include "navbar.php";
                     map.setZoom(18);
                 });
             }
+        }
+
+        function searchAddress() {
+            var start = document.getElementById("start");
+            var end = document.getElementById("end");
+            var requestStart = {
+                query: start.value,
+                fields: ['name']
+            };
+            var requestEnd = {
+                query: end.value,
+                fields: ['name', 'geometry']
+            };
+            var service = new google.maps.places.PlacesService(map);
+            service.findPlaceFromQuery(requestStart, function(results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    start.value = results[0].name
+                }else {
+                    alert("no result for start")
+                }
+            });
+            service.findPlaceFromQuery(requestEnd, function(results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    end.value = results[0].name
+                }else {
+                    alert("no result for end")
+                }
+            });
+
+            //lancement de l'itinéraire
+            directionsService = new google.maps.DirectionsService;
+            directionsDisplay = new google.maps.DirectionsRenderer;
+            directionsDisplay.setMap(map);
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+        }
+
+        function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+            directionsService.route({
+                origin: document.getElementById('start').value,
+                destination: document.getElementById('end').value,
+                travelMode: 'DRIVING'
+            }, function(response, status) {
+                if (status === 'OK') {
+                    directionsDisplay.setDirections(response);
+                } else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            });
         }
 
     </script>
