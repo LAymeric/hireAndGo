c92d441401132f4dd00b01c9de6d0232c35021e1
LAymeric <aymeric.larvet@gmail.com>
2019-03-30 20:31:54 +0100

    reservation via parameters

--- script/saveReservation.php
+++ script/saveReservation.php
@@ -13,25 +13,10 @@ ini_set('display_startup_errors', 1);
     && !empty($_POST["arrival_postal_code"])
     && !empty($_POST["arrival_city"])
   ) {
-      echo "test";
     // Database connection
     $connection = connectDB();
 
-
     $error = false;
-
-    // Check values one by one
-
-    // name length: min 3 max 60
-    /*if (strlen($_POST["reservation_date"]) < 2 || strlen($_POST["name"]) > 60) {
-        $error = true;
-    }
-
-    // firstname length: min 3 max 60
-
-    if (strlen($_POST["firstname"]) < 2 || strlen($_POST["firstname"]) > 60) {
-         $error = true;
-    }*/
 
     if (strlen($_POST["departure_address"]) < 7 || strlen($_POST["departure_address"]) > 255) { 
          $error = true;
@@ -60,24 +45,13 @@ ini_set('display_startup_errors', 1);
 
     if ($error) {
         var_dump($_POST);
-        echo "test2";
       //header("Location: ../index.php");
     } 
 
     // Else => insertion in database
 
     else {
-
-        echo "test3";
-        $reservation = new Reservation($_POST["reservation_time"],
-            $_POST["reservation_date"],
-            $_POST["departure_address"],
-            $_POST["departure_postal_code"],
-            $_POST["departure_city"],
-            $_POST["arrival_address"],
-            $_POST["arrival_postal_code"],
-            $_POST["arrival_city"]);
-        $reservation->addReservation($_POST["reservation_date"],
+        $reservation = new Reservation($_POST["reservation_date"],
             $_POST["reservation_time"],
             $_POST["departure_address"],
             $_POST["departure_postal_code"],
@@ -85,6 +59,7 @@ ini_set('display_startup_errors', 1);
             $_POST["arrival_address"],
             $_POST["arrival_postal_code"],
             $_POST["arrival_city"]);
+        $reservation->addReservation();
     }
 
   } else {
