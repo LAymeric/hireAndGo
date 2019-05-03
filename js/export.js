c968339809dfc25fb0546e7036848d3160f05a5d
hermance <hermancedornier@gmail.com>
2019-05-03 17:42:56 +0200

    export

--- js/export.js
+++ js/export.js
@@ -1,3 +1,32 @@
 function downloadXls(){
+    $.ajax({ url: 'http://localhost:8080/api/command/export',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                type: 'GET',
+                dataType: "json",
+                success : function(code, status){
+                    downloadExcel(code)
 
+                },
+
+                error : function(result, status, error){
+                    errorPopup()
+                },
+
+                complete : function(result, status){
+                }
+
+            });
+}
+
+function downloadExcel(file) {
+    const linkSource = `data:application/vnd.ms-excel;base64,${file.base64}`;
+    const downloadLink = document.createElement("a");
+    const fileName = file.fileName;
+
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    downloadLink.click();
 }
